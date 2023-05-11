d5327d3bf6720222066d9f89eb6d483679484ab8
Dave Honneffer <pearofducks@gmail.com>
2023-05-11 11:39:53 +0200

    New

---
+++ index.js
@@ -0,0 +1,23 @@
+import path from 'node:path'
+
+const PLACEHOLDER = '@itsy-css-asset;'
+
+export default function() {
+  let _config
+  return {
+    name: 'itsy:css-asset',
+    enforce: 'post',
+    apply: 'build',
+    configResolved: (config) => _config = config,
+    generateBundle(_, bundle) {
+      const bundleFiles = Object.values(bundle)
+      const relevantFiles = bundleFiles.filter(f => f.type === 'chunk').filter(f => f.code.match(PLACEHOLDER))
+      if (!relevantFiles.length) return
+      const base = _config.env.BASE_URL
+      const withBase = p => path.posix.join(base, p)
+      const cssFiles = bundleFiles.filter(f => f.type === 'asset' && f.fileName.includes('.css')).map(f => withBase(f.fileName))
+      const replacementString = cssFiles.map(s => `@import '${s}';`).join('')
+      for (const file of relevantFiles) file.code = file.code.replace(PLACEHOLDER, replacementString)
+    }
+  }
+}
