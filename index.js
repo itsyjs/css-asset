7a169ffea0cbd5990369c823513a379561739b4f
Dave Honneffer <pearofducks@gmail.com>
2023-06-22 14:54:59 +0200

    Fix

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import path from 'node:path'
-
 const PLACEHOLDER = '@itsy-css-asset;'
 
 export default function() {
@@ -14,7 +12,7 @@ export default function() {
       const relevantFiles = bundleFiles.filter(f => f.type === 'chunk').filter(f => f.code.match(PLACEHOLDER))
       if (!relevantFiles.length) return
       const base = _config.env.BASE_URL
-      const withBase = p => path.posix.join(base, p)
+      const withBase = p => new URL(p, base).href
       const cssFiles = bundleFiles.filter(f => f.type === 'asset' && f.fileName.includes('.css')).map(f => withBase(f.fileName))
       const replacementString = cssFiles.map(s => `@import '${s}';`).join('')
       for (const file of relevantFiles) file.code = file.code.replace(PLACEHOLDER, replacementString)
